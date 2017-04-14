3413f659e708a20b107391274edb1021acd46a51
Cory Foy <github@cornetdesign.com>
2017-04-14 00:45:05 -0400

    Corrects issue where 1s parsing didn't match client file

--- test/lib/parser.test.js
+++ test/lib/parser.test.js
@@ -25,7 +25,7 @@ module.exports = {
 
   parsingAOneTokenReturnsTheNumberOne: function() {
     var token = new Token();
-    token.positions = [" ", " ", "|"
+    token.positions = [" ", " ", " "
                      , " ", " ", "|"
                      , " ", " ", "|"];
     var parser = new Parser();
