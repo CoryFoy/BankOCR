aac9b8aa8e51dffcb279742749fa608ce3f1ab0d
Cory Foy <github@cornetdesign.com>
2017-04-13 22:47:26 -0400

    Scanner now creates multiple tokens from input strings

--- test/lib/scanner.test.js
+++ test/lib/scanner.test.js
@@ -38,5 +38,26 @@ module.exports = {
     var token = scanner.tokens[0];
     expectedTokenPositions = ["1","2","3","4","5","6","7","8","9"];
     assert.deepEqual(expectedTokenPositions, token.positions);
-  }
+  },
+
+  scanningAStringProducesMultipleTokensForMultipleCharacters: function() {
+    var multiToken = "      \n      \n      \n      ";
+    var scanner = new Scanner(multiToken);
+    scanner.scan();
+    assert.equal(2, scanner.tokens.length);
+  },
+
+  scanningAStringPutsTheCharactersForMultipleTokensInTheRightPosition: function() {
+    var singleToken = "123abc\n456def\n789ghi\n   ";
+    var scanner = new Scanner(singleToken);
+    scanner.scan();
+    var firstToken = scanner.tokens[0];
+    var secondToken = scanner.tokens[1];
+
+    expectedFirstTokenPositions = ["1","2","3","4","5","6","7","8","9"];
+    expectedSecondTokenPositions = ["a","b","c","d","e","f","g","h","i"];
+
+    assert.deepEqual(expectedFirstTokenPositions, firstToken.positions);
+    assert.deepEqual(expectedSecondTokenPositions, secondToken.positions);
+  },
 }
