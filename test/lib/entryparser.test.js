36ee29fa541e7bdc7711a76485cba1d0d58fcd4e
Cory Foy <github@cornetdesign.com>
2017-04-14 01:17:40 -0400

    Entry Parser now returns an account object instead of just an account number

--- test/lib/entryparser.test.js
+++ test/lib/entryparser.test.js
@@ -1,11 +1,13 @@
 var assert = require('assert');
 var EntryParser = require('../../lib/entryparser');
+var Account = require('../../lib/account');
 
 module.exports = {
   parserReturnsResultsForParsedTokenGroup: function() {
     var testFileGroupEntry = "    _  _     _  _  _  _  _ \n  | _| _||_||_ |_   ||_||_|\n  ||_  _|  | _||_|  ||_| _|\n";
     var entryParser = new EntryParser();
-    assert.equal("123456789", entryParser.parseEntry(testFileGroupEntry));
+    var expectedAccount = new Account("123456789");
+    assert.deepEqual(expectedAccount, entryParser.parseEntry(testFileGroupEntry));
   }
 
 }
